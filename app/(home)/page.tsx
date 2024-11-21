0797dd060ef316253e5ef6329eca2e89b4a5c7e1
Luiie <dbsldkfls@ewhain.net>
2024-11-21 20:29:07 +0900

    6th, data fetching using React

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,14 +1,24 @@
-import Navigation from "../../components/navigation";
+"use client"
 
-export const metadata = {
-    title: "Home"
-  };
-  
+import { useEffect, useState } from "react";
+
 export default function Home(){
+    const [isLoading, setIsLoading] = useState(true);
+    const [furnitures, setFurnitures] = useState([]);
+    const getFurnitures = async() => {
+        const response = await fetch("https://5f210aa9daa42f001666535e.mockapi.io/api/products");
+        const json = await response.json();
+        setFurnitures(json);
+        setIsLoading(false);
+    };
+    useEffect(() => {
+        getFurnitures();
+    }, []);
     return (
         <div>
             <h1>Hello Next.js</h1>
-            <h2>Welcome Home</h2>            
+            <h2>Welcome Home</h2>
+            <div>{isLoading ? "Loading" : JSON.stringify(furnitures)}</div>
         </div>
     );
 }
