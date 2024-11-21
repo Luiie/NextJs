3921cd6167078b612d47e3fbd103429a7f8a4335
Luiie <dbsldkfls@ewhain.net>
2024-11-21 21:07:06 +0900

    7th, data fetching at Server Side

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,24 +1,22 @@
-"use client"
+export const metadata = {
+    title: "Home",
+};
 
-import { useEffect, useState } from "react";
+const URL = "https://5f210aa9daa42f001666535e.mockapi.io/api/products";
 
-export default function Home(){
-    const [isLoading, setIsLoading] = useState(true);
-    const [furnitures, setFurnitures] = useState([]);
-    const getFurnitures = async() => {
-        const response = await fetch("https://5f210aa9daa42f001666535e.mockapi.io/api/products");
-        const json = await response.json();
-        setFurnitures(json);
-        setIsLoading(false);
-    };
-    useEffect(() => {
-        getFurnitures();
-    }, []);
+async function getFurnitures() {
+    const response = await fetch(URL);
+    const json = await response.json();
+    return json;
+}
+
+export default async function Home(){
+    const furnitures = await getFurnitures();
     return (
         <div>
             <h1>Hello Next.js</h1>
             <h2>Welcome Home</h2>
-            <div>{isLoading ? "Loading" : JSON.stringify(furnitures)}</div>
+            <div>{JSON.stringify(furnitures)}</div>
         </div>
     );
 }
